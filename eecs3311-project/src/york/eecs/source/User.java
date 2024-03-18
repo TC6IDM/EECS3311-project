a6b0cc2f5cb87f26ec34da7dbdd4a0428a1d8216
TC6IDM <67088794+TC6IDM@users.noreply.github.com>
2024-03-18 17:49:51 -0400

    Add files via upload

---
+++ eecs3311-project/src/york/eecs/source/User.java
@@ -0,0 +1,20 @@
+/**
+ * @author wang & you
+ */
+package york.eecs.source;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public abstract class User {
+    private String username;
+    private String userType;
+    
+    public User(String username, String userType) {
+        this.username = username;
+        this.userType = userType;
+    }
+  
+}
+
+//Test 
---
+++ eecs3311-project/test/york/eecs/test/TestCases.java
@@ -0,0 +1,22 @@
+package york.eecs.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import york.eecs.source.User;
+
+public class TestCases {
+	
+	 @Before
+	 public void setUp() throws Exception {
+//		 bubble = new User();
+	 }
+	 
+	 @Test
+	 public void testUserCreation() {
+		 	 
+	 }
+}
